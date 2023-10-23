ec6e626258c7117584c91291215048e7bef123aa
Krzysztof Laudanski <laudanski.krzysztof86@gmail.com>
2023-10-23 15:03:04 +0200

    fix bug with server.mjs

--- server.mjs
+++ server.mjs
@@ -1,15 +1,21 @@
 import jsonServer from 'json-server';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const server = jsonServer.create();
 const router = jsonServer.router('build/db/app.json');
 const middlewares = jsonServer.defaults({
   static: 'build',
   noCors: true
 });
+
 const port = process.env.PORT || 3131;
 server.use(middlewares);
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1'
-}));
+server.use('/api', router);
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 
-server.use(router);
 server.listen(port);
